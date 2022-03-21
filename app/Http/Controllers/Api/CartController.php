e440cafe702969349fb906442029d4c78152c979
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-03-22 06:58:52 +0700

    Add: if product exist in cart

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -11,9 +11,9 @@ use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $carts = Cart::with('product')->where('user_id', auth()->user()->id)->get()->groupBy('product_id');
+        $carts = Cart::with('product')->where('user_id', $request->user()->id)->get()->groupBy('product_id');
 
         $total = 0;
 
@@ -35,9 +35,18 @@ class CartController extends Controller
 
         if($validator->fails()) return ResponseHelper::error($validator->errors(), 'Product Not Found', 404);
 
+        $product_exist = Cart::where('user_id', $request->user()->id)->where('product_id', $request->product_id)->first();
+
+        if ($product_exist) {
+            $product_exist->update([
+                'qty' => $product_exist->qty + 1
+            ]);
+        }
+
         Cart::create([
             'user_id' => auth()->user()->id,
             'product_id' => $request->product_id,
+            'qty' => 1
         ]);
 
         return ResponseHelper::success();
